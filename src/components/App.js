06ce443333c700b9e81e130ff8e3f6189b302d5c
iannjau <lantanacreations@gmail.com>
2024-05-06 10:43:14 +0300

    add my file

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,45 @@
 import video from "../data/video.js";
+import React, {useState} from "react";
 
 function App() {
   console.log("Here's your data:", video);
+
+ const [like,setLike] =useState(10000000)
+ const [Dislike,setdisLike] =useState(10000)
+
+ const [likeactive,setLikeactive] =useState(false)
+ const [Dislikeactive,setDisLikeactive] =useState(false)
+
+function likef(){
+  if(likeactive){
+    setLikeactive(false)
+    setLike(like -1)
+  }else{
+    setLikeactive(true)
+    setLike(like+1)
+    if(Dislikeactive){
+      setDisLikeactive(false)
+      setLike(like +1)
+      setdisLike(Dislike +1)
+    }
+  }
+  
+}
+function dislikef(){
+  if(Dislikeactive){
+    setDisLikeactive(false)
+    setdisLike(Dislike -1)
+  }else{
+    setDisLikeactive(true)
+    setdisLike(like+1)
+    if(likeactive){
+      setDisLikeactive(false)
+      setdisLike(Dislike +1)
+      setLike(like +1)
+    }
+  }
+}
+
 
   return (
     <div className="App">
@@ -13,6 +51,15 @@ function App() {
         allowFullScreen
         title="Thinking in React"
       />
+      <p>
+        NEVER GONNA GIVE UP - Rick Astley
+        </p>
+        <p>
+        1,532,449,264 views | Oct 25, 2009
+        </p>
+      <p><button className="like" onClick={likef}>like {like}</button>
+      <button  className="dislike"  onClick={dislikef}>Dislike {Dislike}</button></p>
+      <p><button>Hidden comments</button></p>
     </div>
   );
 }
